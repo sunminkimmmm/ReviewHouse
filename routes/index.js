415b710ea58acd1a3e773946a4afc641211ab4bb
sunminkimmmm <minmax24@naver.com>
2019-05-16 00:42:27 +0900

    first commit

---
+++ routes/index.js
@@ -0,0 +1,461 @@
+var express = require('express');
+var router = express.Router();
+var pool = require('../config');
+//var elasticsearch = require('@elastic/elasticsearch');
+// var elasticsearch = require('elasticsearch');
+/*var esclient = new elasticsearch.Client({
+  host: 'localhost:3000'
+});
+esclient.search({
+  index: 'social-*',
+  body: {
+    query: {
+      match: { message: 'myProduct' }
+    },
+    aggs: {
+      top_10_states: {
+        terms: {
+            field: 'state',
+            size: 10
+        }
+      }
+    }
+  }
+}
+).then(function (response) {
+    var hits = response.hits.hits;
+}
+);
+*/
+var multer = require('multer')
+
+var _storage = multer.diskStorage({
+  destination: function (req, file, cb){
+    cb(null, 'public/images');
+  },
+  filename: function(req, file, cb) {
+    cb(null, file.originalname)
+  }
+})
+var upload = multer({storage:_storage});
+
+
+/* 메인 페이지 */
+router.get('/recently', function(req, res, next) {
+  pool.getConnection((err,conn)=>{
+    if(err){
+      throw err;
+    }
+    var sql = "SELECT * FROM house ORDER BY houseIdx DESC LIMIT 3";
+    conn.query(sql,(err,row)=>{
+      conn.release();
+      if(err){
+        throw err;
+      }
+      if (row.length === 0) {
+        res.send(300, {
+          result: 0,
+          msg: "failed"
+        });
+      } else{                                 
+        res.send(200, {
+          result: 1,
+          check: 0,
+          data: row
+        })
+      }
+    })
+  })
+  
+});
+
+
+//집주인
+/*집 등록하기 */
+router.post('/houseRegister', upload.single('photo'),function (req, res, next) {
+  var postData = req.body;
+  var imgurl = 'images/' + req.file.originalname;
+  pool.getConnection(function (err, conn) {
+    if (err) throw err;
+    var sql = "INSERT INTO house (housePic, housePrice, houseSpace, houseComment, houseAddress, userMail) VALUES (?, ?, ?, ?, ?, ?);"
+    conn.query(sql, [imgurl, postData.housePrice, postData.houseSpace, postData.houseComment, postData.houseAddress, postData.userMail], function (err, row) {
+        conn.release()
+          if (err) throw err;
+          res.send(200,{
+          result:1
+
+          })  
+    });
+  }) 
+
+})
+
+/*집 삭제 */
+router.get('/houseDelete/:houseIdx', function (req, res, next) {
+  var houseIdx = req.params.houseIdx;
+  pool.getConnection((err, conn) => {
+      if (err) {
+          throw err;
+      }
+      var sql = "DELETE FROM house WHERE houseIdx = ?";
+      conn.query(sql,[houseIdx], function(err){
+          if(err){
+          throw err;
+          }
+          res.send(200,{
+            result:1
+          })
+      });
+  })
+});
+
+
+/*집 수정 */
+router.post('/houseUpdate/:houseIdx',upload.single('photo'),function(req,res,next){
+  var houseIdx = req.params.houseIdx;
+  var postData = req.body;
+  var imgurl = 'images/' + req.file.originalname;
+  pool.getConnection((err,conn)=>{
+    if(err){
+      throw err;
+    }
+    var sql = `UPDATE house SET
+      housePic = ?,
+      housePrice = ?,
+      houseSpace = ?,
+      houseComment = ?,
+      houseAddress = ?
+      WHERE houseIdx = ? AND userMail = ?`;
+    conn.query(sql,[imgurl,postData.housePrice,postData.houseSpace,postData.houseComment,postData.houseAddress,houseIdx,postData.userMail],(err,row)=>{
+    conn.release();
+    if(err){
+      throw err;
+      }
+      console.log(row);
+      res.send(200,{
+        result:1
+      })
+        
+    })
+  
+  })
+
+})
+
+/*마이페이지(집주인) */
+router.get('/ownerMypage/:userMail',function(req,res,next){
+ var userMail = req.params.userMail;
+ pool.getConnection((err,conn)=>{
+   if(err){
+     throw err;
+   }
+   var sql = "SELECT * FROM user WHERE userMail = ?";
+   conn.query(sql,[userMail],(err,row)=>{
+    if(err){
+      throw err;
+    }
+    var sql = "SELECT * FROM user, house WHERE user.userMail = house.userMail AND user.userMail = ?";
+    conn.query(sql,[userMail],(err,row)=>{
+      conn.release();
+      if(err){
+        throw err;
+      }
+      if (row.length === 0) {
+        res.send(300, {
+          result: 0,
+          msg: "failed"
+        });
+      } else{                                 
+        res.send(200, {
+          result: 1,
+          data: row
+        })
+      }
+    })
+    
+  })
+ })
+})
+
+
+/*집 상세보기(집주인)*/
+router.get('/houseView/:houseIdx', function(req,res,next){
+  var houseIdx = req.params.houseIdx;
+  pool.getConnection((err,conn)=>{
+    if(err){
+      throw err;
+    }
+    var sql = "SELECT * FROM house WHERE houseIdx = ?";
+    conn.query(sql,[houseIdx],(err,row)=>{
+      if(err){
+        throw err;
+      }
+      var sql = "SELECT * FROM house,review, user WHERE house.houseIdx = review.houseIdx AND review.userMail = user.userMail AND house.houseIdx = ?"
+      conn.query(sql,[houseIdx],(err,row)=>{
+        conn.release();
+        if (err) {
+          throw err;
+      }
+      if (row.length === 0) {
+        res.send(300, {
+          result: 0,
+          msg: "failed"
+        });
+      } else{                                 
+        res.send(200, {
+          result: 1,
+          data: row
+        })
+      }
+      })
+    })
+  })
+})
+
+
+/*사용자 */
+/*리뷰 등록 */
+router.post('/reviewRegister', function (req, res, next) {
+  var postData = req.body;
+  pool.getConnection(function (err, conn) {
+    if (err) throw err;
+    var sql = "INSERT INTO review (userMail, reviewComment, houseIdx) VALUES (?, ?, ?);"
+    conn.query(sql, [postData.userMail, postData.reviewComment, postData.houseIdx], function (err, row) {
+        conn.release()
+          if (err) throw err;
+          res.send(200,{
+          result:1
+
+          })  
+    });
+  }) 
+})
+
+/*리뷰 삭제 */
+router.get('/reviewDelete/:houseIdx/:userMail', function (req, res, next) {
+  var houseIdx = req.params.houseIdx;
+  var userMail = req.params.userMail;
+  pool.getConnection((err, conn) => {
+      if (err) {
+          throw err;
+      }
+      var sql = "DELETE FROM review WHERE houseIdx = ? AND userMail = ?";
+      conn.query(sql,[houseIdx,userMail], function(err){
+          if(err){
+          throw err;
+          }
+          res.send(200,{
+            result:1
+          })
+      });
+  })
+});
+
+/*리뷰 수정 */
+router.post('/reviewUpdate/:houseIdx/:userMail',function(req,res,next){
+  var houseIdx = req.params.houseIdx;
+  var userMail = req.params.userMail;
+  var postData = req.body;
+  pool.getConnection((err,conn)=>{
+    if(err){
+      throw err;
+    }
+    var sql = `UPDATE review SET reviewComment = ? WHERE houseIdx = ? AND userMail = ?`;
+    conn.query(sql,[postData.reviewComment,houseIdx,userMail],(err,row)=>{
+    conn.release();
+    if(err){
+      throw err;
+      }
+      console.log(row);
+      res.send(200,{
+        result:1
+      })
+        
+    })
+  
+  })
+
+})
+
+/*사용자 리뷰 목록(사용자 마이페이지 내에서 이동) */
+router.get('/reviewList/:userMail',function(req,res,next){
+  var userMail = req.params.userMail;
+  pool.getConnection((err,conn)=>{
+    if(err){
+      throw err;
+    }
+    var sql = "SELECT * FROM user WHERE userMail = ?";
+    conn.query(sql,[userMail],(err,row)=>{
+     if(err){
+       throw err;
+     }
+     var sql = "SELECT * FROM user, review WHERE user.userMail = review.userMail AND user.userMail = ?";
+     conn.query(sql,[userMail],(err,row)=>{
+       conn.release();
+       if(err){
+         throw err;
+       }
+       if (row.length === 0) {
+         res.send(300, {
+           result: 0,
+           msg: "failed"
+         });
+       } else{                                 
+         res.send(200, {
+           result: 1,
+           data: row
+         })
+       }
+     })
+     
+   })
+  })
+ })
+
+ /*좋아요*/
+ router.post('/houseLike', function (req, res, next) {
+  var postData = req.body;
+  pool.getConnection(function (err, conn) {
+    if (err) throw err;
+    var sql = "INSERT INTO favorite (userMail, houseIdx, favoriteCheck) VALUES (?, ?, ?);"
+    conn.query(sql, [postData.userMail, postData.houseIdx, postData.favoriteCheck], function (err, row) {
+        conn.release()
+          if (err) throw err;
+          res.send(200,{
+          result:1
+
+          })  
+    });
+  }) 
+})
+
+/*마이페이지(사용자, 좋아요 누른 집 게시물 목록이 나옴) */
+router.get('/userMypage/:userMail',function(req,res,next){
+  var userMail = req.params.userMail;
+  pool.getConnection((err,conn)=>{
+    if(err){
+      throw err;
+    }
+    var sql = "SELECT * FROM user WHERE userMail = ?";
+    conn.query(sql,[userMail],(err,row)=>{
+     if(err){
+       throw err;
+     }
+     var sql = "SELECT * FROM user, house, favorite WHERE user.userMail = favorite.userMail AND favorite.houseIdx = house.houseIdx AND user.userMail = ?";   
+     conn.query(sql,[userMail],(err,row)=>{
+       conn.release();
+       if(err){
+         throw err;
+       }
+       if (row.length === 0) {
+         res.send(300, {
+           result: 0,
+           msg: "failed"
+         });
+       } else{                                 
+         res.send(200, {
+           result: 1,
+           data: row
+         })
+       }
+     })
+     
+   })
+  })
+ })
+
+/*집, 리뷰 검색 */
+router.post('/houseSearch/:searchWord', function(req, res, next) {
+  var searchWord = req.params.searchWord;
+  pool.getConnection((err,conn)=>{
+    if(err){
+      throw err;
+    }
+    var sql = "SELECT * FROM house,review WHERE house.userMail = review.userMail AND house.housePrice = ? AND house.houseSpace = ? AND house.houseAddress = ? AND review.reviewComment LIKE '%searchWord%' = ? ";
+    conn.query(sql,[req.body.housePrice,req.body.houseSpace,req.body.houseAddress,searchWord],(err,row)=>{
+      conn.release();
+      if(err){
+        throw err;
+      }
+      if (row.length === 0) {
+        res.send(300, {
+          result: 0,
+          msg: "failed"
+        });
+      } else{                                 
+        res.send(200, {
+          result: 1,
+          data: row
+        })
+      }
+    })
+  })
+  
+});
+ 
+
+/* 로그인 요청 */
+router.post('/login', function (req, res, next) {
+  var postData = req.body;
+  pool.getConnection((err, conn) => {
+    if (err) throw err;
+    var sql = "SELECT * FROM user WHERE userMail = ? AND userPassword = ? AND userCheck = ?";
+    conn.query(sql, [postData.userMail, postData.userPassword, postData.userCheck], (err, row) => {
+      if (err) {
+        res.send(300, {
+          result: 0,
+          msg: 'DB Error'
+        });
+      }
+      if (row.length === 0) {
+        res.send(300, {
+          result: 0,
+          msg: "failed"
+        });
+      } else if(postData.userCheck === 0) {         //집주인일때
+        res.send(200, {
+          result: 1,
+          check: 1,
+          data: row
+        })
+      } else{                                  //사용자일때
+        res.send(200, {
+          result: 1,
+          check: 0,
+          data: row
+        })
+      }
+    });
+  })
+
+});
+
+
+
+/*회원가입 요청*/
+router.post('/join', function (req, res, next) {
+  pool.getConnection(function (err, conn) {
+    if (err) throw err;
+    var sql = "SELECT * FROM user WHERE userMail=?"
+    conn.query(sql, [req.body.userMail], function (err, row) {
+      if (err) throw err;
+      if (row.length === 0) {
+        var sql = "INSERT INTO user VALUES (?, ?, ?, ?);"
+        conn.query(sql, [req.body.userMail, req.body.userPassword, req.body.userName, req.body.userCheck], function (err, row) {
+          conn.release()
+          if (err) throw err;
+          res.send(200,{
+            result:1
+          })
+        });
+      } else {
+        res.send("중복")
+      }
+    });
+  })
+  
+
+})
+
+
+module.exports = router;
